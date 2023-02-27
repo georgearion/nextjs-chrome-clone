2833c2eb2b67b3f5da0652ae7395e679245958a7
George ARION <georgearion@gmail.com>
2023-02-27 14:39:29 +0700

    feat: implement tailwind css

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,20 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    // required when using css modules
+    'storybook-css-modules-preset',
+    {
+      /**
+       * Fix Storybook issue with PostCSS@8
+       * @see https://github.com/storybookjs/storybook/issues/12668#issuecomment-773958085
+       */
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
   framework: '@storybook/react',
   core: {
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import './globals.css';
 import { NextPageWithLayout } from './page';
 
 interface AppPropsWithLayout extends AppProps {
